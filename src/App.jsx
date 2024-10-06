e84469ab30b75eb0eba8074fce15dffd14f176f5
Deepika Teegapuram <dteegapuram@gmail.com>
2024-10-05 20:30:56 -0600

    added state to handle the project creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setProjectsState]= useState({
+    selectedProjectId: undefined,
+    projects:[]
+  });
+
+  function handleStartAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId : null,
+      }
+    });
+  }
   return (
    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <NoProjectSelected onStartAddProject={handleStartAddProject}/>
    </main>
   );
 }

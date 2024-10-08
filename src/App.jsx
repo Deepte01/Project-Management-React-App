706b7617645727cd68a3f3f81e476b51dc9ebdc1
Deepika Teegapuram <dteegapuram@gmail.com>
2024-10-08 17:05:24 -0600

    added a new method to delete the project.

--- src/App.jsx
+++ src/App.jsx
@@ -54,10 +54,26 @@ function App() {
      })
   }
 
+  function handleDeleteProject()
+  {
+
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId : undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    });
+  }
+
   console.log(projectState);
 
-  let selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
-  let content = <SelectedProject project={selectedProject}/>;
+  let selectedProject = projectState.projects.find(project => 
+    project.id === projectState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+
   if(projectState.selectedProjectId === null)
   {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
